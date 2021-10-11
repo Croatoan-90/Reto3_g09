0956e7bddd66c3f1c25d1dead36aee8c38d3076f
fabianhernandezmt <91509505+fabianhernandezmt@users.noreply.github.com>
2021-10-10 19:03:40 -0500

    nuevo repositorio para pruebas

--- reto3/src/main/java/com/g09/reto3/repository/ClientsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.g09.reto3.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.g09.reto3.entity.Clients;
-
-
-public interface ClientsRepository extends CrudRepository<Clients, Long>{
-    
-}
--- reto3/src/main/java/com/g09/reto3/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.g09.reto3.repository;
-
-import com.g09.reto3.entity.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<Message, Long>{
-    
-}
