481cb405776141aef4e5e215ef300cbb9bd9b662
fabianhernandezmt <91509505+fabianhernandezmt@users.noreply.github.com>
2021-10-25 10:19:10 -0500

    frontend_Reto3

--- reto3/src/main/java/com/g09/reto3/repository/AdminUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.g09.reto3.repository;
-
-import com.g09.reto3.entity.AdminUser;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminUserRepository extends CrudRepository<AdminUser, Long>{
-    
-}
